5350bea28254d78bf55a37e157cb598506b5c2d1
Hasan Demir <150049291+hasandemir34@users.noreply.github.com>
2026-01-19 17:14:53 +0300

    first commit

---
+++ Models/Blog.cs
@@ -0,0 +1,7 @@
+public class Blog
+{
+    public int Id { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public string Content { get; set; } = string.Empty;
+    public DateTime CreatedDate { get; set; } = DateTime.Now;
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace mywebsite.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
