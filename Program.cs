f7b02e7844ec182ba6a6dfb766e8620d4bb2d1dd
Hasan Demir <150049291+hasandemir34@users.noreply.github.com>
2026-01-19 18:14:32 +0300

    bazı düzeltmeler

--- Program.cs
+++ Program.cs
@@ -1,24 +1,26 @@
+using Microsoft.EntityFrameworkCore; // - UseNpgsql için gerekli
+using mywebsite.Data; // - AppDbContext'in bulunduğu klasör
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Servisleri ekle
 builder.Services.AddControllersWithViews();
 
+// Veritabanı bağlantısı
+builder.Services.AddDbContext<AppDbContext>(options => 
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-
-builder.Services.AddDbContext<AppDbContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// HTTP istek boru hattını yapılandır
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
 app.UseRouting();
-
 app.UseAuthorization();
 
 app.MapStaticAssets();
@@ -27,6 +29,5 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}")
     .WithStaticAssets();
-
 
 app.Run();
