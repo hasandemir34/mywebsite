85b7b84a027ebcd5ecfac364b2d271986c69334a
Hasan Demir <150049291+hasandemir34@users.noreply.github.com>
2026-01-25 18:22:42 +0300

    medium eklendi

--- obj/Debug/net10.0/mywebsite.AssemblyInfo.cs
+++ obj/Debug/net10.0/mywebsite.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("mywebsite")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+236c67bc68139e059d462a3803815595742c53d6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5399022216f9560cae291df6b1163ebb8b9705e2")]
 [assembly: System.Reflection.AssemblyProductAttribute("mywebsite")]
 [assembly: System.Reflection.AssemblyTitleAttribute("mywebsite")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
